2f14f39129d9438370c99f325ac01807a7e4903c
Minh Quan <150663837+mrorabang@users.noreply.github.com>
2025-01-04 00:12:03 +0700

    mq update lung tung

--- eprojectclient/src/routes/routes.js
+++ eprojectclient/src/routes/routes.js
@@ -1,39 +1,35 @@
 // import config from '../config/routes';
 
+import AwardReceived from "../pages/AwardReceived";
 import Contest from "../pages/Contest";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 
-
-
 export const publicRoutes = [
-    {
-        path: "/",
-        element: <Home/>,
-        
-    },
-    {
-
-        path: "/login",
-        element: <Login/>,
-    },
-
-        path: "/contest",
-        element: <Contest/>,
-        
-    },
-
-        
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/contest",
+    element: <Contest />,
+  },
+  {
+    path: "/AwardReceived",
+    element: <AwardReceived />,
+  },
 ];
 export const privateRoutes = [
-    // {
-    //     path: config.home.path,
-    //     component: config.home.component,
-    // },
-    // {
-    //     path: config.home.path,
-    //     component: config.home.component,
-    // },
-
-
+  // {
+  //     path: config.home.path,
+  //     component: config.home.component,
+  // },
+  // {
+  //     path: config.home.path,
+  //     component: config.home.component,
+  // },
 ];
