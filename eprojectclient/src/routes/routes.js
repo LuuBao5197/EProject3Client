fa16341c45d1f00bc41b9095c0f8afcebe05241d
LuuBao5197 <baolpb5197@gmail.com>
2025-01-04 19:39:31 +0700

    Delete import trùng in route.routejs

--- eprojectclient/src/routes/routes.js
+++ eprojectclient/src/routes/routes.js
@@ -1,16 +1,12 @@
 // import config from '../config/routes';
-
 import { AdminConfig, TeacherConfig } from "../config/routes";
-
 // import Contest from "../pages/Contest";
 import EditProfile from "../pages/PublicPages/EditProfile";
-
 import Contest from "../pages/StudentPages/Contest";
-
 import Home from "../pages/PublicPages/Home";
 import Login from "../pages/PublicPages/Login";
 import AwardReceived from "../pages/StudentPages/AwardReceived";
-import EditProfile from "../pages/PublicPages/EditProfile";
+
 
 export const publicRoutes = [
     {
