2d157113cdb250e91a0c89eb14bbc76cefd3429b
Minh Quan <150663837+mrorabang@users.noreply.github.com>
2025-01-22 23:33:24 +0700

    OK delte du thua

--- src/API/getStudentIdDemo.js
+++
@@ -1,6 +0,0 @@
-export const getStudentIdDemo = ()=>{
-    //ID student demo
-    //thay thế bằng id đăng nhập
-    let id = 1;
-    return id;
-}
--- src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import productReducer from './productSlice';
-
-const myStore = configureStore({
-    reducer:{
-        products:productReducer
-    }
-})
-export default myStore;
